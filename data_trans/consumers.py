01a6fca75708dfbbd4e53c1944b1d9dbf95c0b98
Sambit Paul <sambitpaul1992@gmail.com>
2019-02-25 16:02:39 +0000

    Made the consumer asynchronous

--- data_trans/consumers.py
+++ data_trans/consumers.py
@@ -2,33 +2,33 @@ import asyncio
 import json
 from django.contrib.auth import get_user_model
 from channels.generic.websocket import JsonWebsocketConsumer
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
+
 from asgiref.sync import async_to_sync
 
-class DataConsumer(JsonWebsocketConsumer):
 
-    def connect(self):
-        print(self.channel_layer)
-        async_to_sync(self.channel_layer.group_add)(
+class DataConsumer(AsyncJsonWebsocketConsumer):
+
+    async def connect(self):
+        await self.channel_layer.group_add(
             "dtrans",
             self.channel_name
         )
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
-        self.channel_layer.group_discard(
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
             "dtrans",
             self.channel_name
         )
 
-    def receive(self, text_data):
-        self.receive_json(self.decode_json(text_data))
+    async def receive(self, text_data):
+        await self.receive_json(await self.decode_json(text_data))
 
-    def receive_json(self, data=None):
+    async def receive_json(self, data=None):
         # Send message to room group
-        print('Type of content:', type(data))
-        print('Content:', data)
         # print('\n\n', text_data, '\n\n')
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             "dtrans",
             {
                 "type": "parse.message",
@@ -36,5 +36,5 @@ class DataConsumer(JsonWebsocketConsumer):
             }
         )
 
-    def parse_message(self, event):
-        self.send_json(event['text'])
+    async def parse_message(self, event):
+        await self.send_json(event['text'])
